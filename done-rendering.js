d474d83d2fbfc8901ce6efa46811149ec18b5e6e
dambrose <dennis@eambrose.com>
2015-04-14 10:12:52 -0400

    make it an UMD module, add setter function

--- done-rendering.js
+++ done-rendering.js
@@ -1,4 +1,12 @@
-;(function($){
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define( [ 'jquery', 'underscore' ], factory );
+    } else {
+        // Browser globals
+        root.doneRendering = factory( root.$, root._ );
+    }
+}(this, function ( $, _ ) {
 
 	$.extend(jQuery.expr[':'], {
 		'done-rendering': function (el) {
@@ -6,4 +14,8 @@
 		}
 	});
 
-})(jQuery);
+	return _.debounce( _.once(function( el ) {
+		$( el ).data('done-rendering', true);
+	}), 1000);
+
+}));
